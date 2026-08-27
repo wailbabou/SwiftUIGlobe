04d35ab3457777aa43577eb0ee17840774cc6e8a
Ouail Bellal <ouailbellal@gmail.com>
2026-08-26 22:40:36 -0400

    Initial Commit

---
+++ SwiftUIGlobe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIGlobe
+//
+//  Created by ouail bellal on 2026-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIGlobe/SwiftUIGlobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGlobeApp.swift
+//  SwiftUIGlobe
+//
+//  Created by ouail bellal on 2026-08-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGlobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
